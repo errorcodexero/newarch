3d9bcb044865f079f9897dde6f7b9a4b362354eb
Matthew Macovsky <macovsky.m@gmail.com>
2018-07-14 19:08:18 -0700

    Move talon initialization to a seperate function

--- xerobase/TankDrive.cpp
+++ xerobase/TankDrive.cpp
@@ -4,21 +4,21 @@
 
 namespace xero {
 	namespace base {
+		void initTalonList(std::list<int>& ids, std::list<TalonPtr>& talons) {
+			//Assuming first id will be master
+			for(int id : ids) {
+				talons.push_back(std::make_shared<TalonSRX>(id));
+				if(talons.size() > 0)
+					talons.back()->Follow(*talons.front());
+			}
+		}
+
 		TankDrive::TankDrive(Robot& robot, std::list<int> left_motor_ids, std::list<int> right_motor_ids) : Drivebase(robot) {
 			//The two sides should always have the same number of motors and at least one motor each
 			assert((left_motor_ids.size() == right_motor_ids.size()) && (left_motor_ids.size() > 0));
 
-			//Assuming first id will be master
-			for(int id : left_motor_ids) {
-				left_motors_.push_back(std::make_shared<TalonSRX>(id));
-				if(left_motors_.size() > 0)
-					left_motors_.back()->Follow(*left_motors_.front());
-			}
-			for(int id : right_motor_ids) {
-				right_motors_.push_back(std::make_shared<TalonSRX>(id));
-				if(right_motors_.size() > 0)
-					right_motors_.back()->Follow(*right_motors_.front());
-			}
+			initTalonList(left_motor_ids, left_motors_);
+			initTalonList(right_motor_ids, right_motors_);
 		}
 
 		void TankDrive::invertMotors(std::list<int> left_motor_ids, std::list<int> right_motor_ids) {
