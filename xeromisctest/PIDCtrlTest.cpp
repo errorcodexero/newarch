3379be16ad294626bca87991af732434449fbb2a
Butch Griffin <bwg@cypress.com>
2018-07-24 09:17:35 -0700

    added google test to external

---
+++ xeromisctest/PIDCtrlTest.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "PIDCtrl.h"
+
+TEST(PIDCtrl, BasicTest)
+{
+    PIDCtrl ctrl ;
+    ctrl.init(1.0, 0.0, 0.0, 0.0) ;
+
+    double v = ctrl.getOutput(100.0, 98.0, 0.1) ;
+    EXPECT_DOUBLE_EQ(v, 2.0) ;
+}
+
