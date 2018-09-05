3543c712d039d5749c89c36d90886edc9aba6e8c
SoftwareA <errorcodexero@gmail.com>
2018-09-04 20:53:48 -0700

    cleaned up liftingcollector and added its children

--- phoenix/liftingcollector/LiftingCollector.h
+++ phoenix/liftingcollector/LiftingCollector.h
@@ -2,10 +2,6 @@
 
 #include <Subsystem.h>
 #include <memory>
-#include <Solenoid.h>
-#include "VictorSP.h"
-#include "DigitalInput.h"
-#include "Encoder.h"
 #include "LiftingCollectorAction.h"
 #include "Collector.h"
 #include "Lifter.h"
@@ -14,18 +10,11 @@ namespace xero {
 	namespace phoenix {
 		/// \brief this is the LiftingCollector subsystem
 		class LiftingCollector : public xero::base::Subsystem {
-			friend class LiftingCollectorCalibrateAction;	
-			friend class LiftingCollectorGoToHeightAction;
-			friend class LiftingCollectorSetDutyCycleAction;
-			friend class LiftingCollectorMaintainHeightAction;
-			friend class LiftingCollectorChangeGearAction;
+
 		public:
-			enum class LiftingCollector_GEAR {LOW, HIGH};
 			LiftingCollector(xero::base::Robot& robot);
 			virtual ~LiftingCollector();
 			virtual void run();
-			double getCurrentHeight();
-			double getCurrentVelocity();
 			virtual void computeState() ;
 
 			std::shared_ptr<Lifter> getLifter(){
@@ -43,23 +32,6 @@ namespace xero {
 		
 		
 		private:
-			int encoder_ticks_;
-			double height_; //Inches
-			double previous_height_; //Inches			//Height in the previous robot loop
-			double velocity_;
-			LiftingCollector_GEAR current_gear_;
-			bool calibrate_flag_;
-			enum class CALIBRATED_STATUS {UNCALIBRATED, CALIBRATED, DECALIBRATED};
-			CALIBRATED_STATUS calibrated_;
-			bool brake_applied_;
-			void calibrate();
-			void setBrake(bool value);
-			void setMotorsDutyCycle(double value);
-			std::vector<std::shared_ptr<frc::VictorSP>> motors_;
-			std::shared_ptr<frc::Encoder> encoder_;
-			std::shared_ptr<frc::Solenoid> brake_solenoid_, gear_solenoid_;
-			std::shared_ptr<frc::DigitalInput> upper_limit_sensor_;
-			std::shared_ptr<frc::DigitalInput> lower_limit_sensor_;
 			std::shared_ptr<Lifter> lifter_;
 			std::shared_ptr<Collector> collector_;
 		};
