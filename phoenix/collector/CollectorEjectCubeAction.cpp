ced83f70b2980cfdde21f5e4f659917ddd22d9db
Meg Hoots <wlnutmeg@gmail.com>
2018-09-06 19:54:28 -0700

    worked on collector eject cube action

--- phoenix/collector/CollectorEjectCubeAction.cpp
+++ phoenix/collector/CollectorEjectCubeAction.cpp
@@ -1,4 +1,8 @@
 #include "CollectorEjectCubeAction.h"
+#include "Collector.h"
+#include "Robot.h"
+#include "intake/IntakeDutyCycleAction.h"
+#include "intake/Intake.h"
 
 using namespace xero::base;
 
@@ -7,10 +11,47 @@ namespace xero {
         
         CollectorEjectCubeAction::CollectorEjectCubeAction(Collector &collector) : CollectorAction(collector){
 
+            Intake& intake = *(getCollector().getIntake());
+
+            outrun_time_ = getCollector().getRobot().getSettingsParser().getDouble("collector:outrun_time");
+
+            intake_out_ = std::make_shared<IntakeDutyCycleAction>(intake, "collector:intake_duty_cycle");
+            intake_off_ = std::make_shared<IntakeDutyCycleAction>(intake, 0);
+
         }
         CollectorEjectCubeAction::~CollectorEjectCubeAction(){
 
         }
+        
+           void CollectorEjectCubeAction::start() {
+               getCollector().getIntake()->setAction(intake_out_);
+               start_time_ =getCollector().getRobot().getTime();
+
+               isdone_ = false;
+           }
+
+            void CollectorEjectCubeAction::run() {
+                double elapsed_time_ = getCollector().getRobot().getTime() - start_time_;
+
+                if (elapsed_time_ > outrun_time_){
+                    getCollector().getIntake()->setAction(intake_off_);
+                    isdone_ = true;
+                }
+            }
+
+            bool CollectorEjectCubeAction::isDone() {
+                
+                return isdone_;
+            }
+
+            void CollectorEjectCubeAction::cancel() {
+                isdone_ = true;
+            }
+
+            std::string CollectorEjectCubeAction::toString() {
+
+            }
+
 
     }
 }
--- phoenix/collector/CollectorEjectCubeAction.h
+++ phoenix/collector/CollectorEjectCubeAction.h
@@ -8,10 +8,34 @@ namespace xero {
         class CollectorEjectCubeAction : public xero::phoenix::CollectorAction {
             public:
                 CollectorEjectCubeAction(Collector &collector);
+
                 virtual ~CollectorEjectCubeAction(); 
+
+                
+                /// \brief Start the calibrate action.
+                virtual void start() ;
+
+                /// \brief Run the duty cycle action.  This method does nothing.            
+                virtual void run() ;
+
+                /// \brief Signals if this action is done, always returs true    
+                virtual bool isDone() ;
+
+                /// \brief Canel the current action, stops the motors and returns true
+                virtual void cancel() ;
+
+                /// \brief Returns a human readable string for the action
+                virtual std::string toString() ;
+
             protected:
                
             private:
+                double start_time_;
+                double outrun_time_;
+                bool isdone_;
+
+                xero::base::ActionPtr intake_out_;
+                xero::base::ActionPtr intake_off_;
                
         };
     }
