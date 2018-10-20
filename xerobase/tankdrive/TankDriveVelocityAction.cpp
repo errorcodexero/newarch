d0784d4b41d05f3ae7629f522eede8db49cca1fc
Meg Hoots <wlnutmeg@gmail.com>
2018-10-20 10:28:32 -0700

    worked on tank drive velocity

--- xerobase/tankdrive/TankDriveVelocityAction.cpp
+++ xerobase/tankdrive/TankDriveVelocityAction.cpp
@@ -1,48 +1,70 @@
 #include "TankDriveVelocityAction.h"
+#include <math.h>
+#include <SettingsParser.h>
+#include "Robot.h"
+#include "basegroups.h"
 
-using namespace xero::base;
+using namespace xero::misc;
 
+
+namespace xero {
+namespace base {
 TankDriveVelocityAction::TankDriveVelocityAction(TankDrive &tank_drive, double target_velocity) : TankDriveAction(tank_drive) {
 	target_velocity_ = target_velocity;
 	is_done_ = false;
 }
 
 void TankDriveVelocityAction::start() {
+	initial_velocity_ = tank_drive_.getVelocity();
 
+	xero::misc::SettingsParser parser = tank_drive_.getRobot().getSettingsParser();
+
+	velocity_pid_.initFromSettingsExtended(parser, "tankdrive:distance_action:velocity_pid");
+	angle_pid_.initFromSettingsExtended(parser, "tankdrive:distance_action:angle_pid", true);
+
+	stall_monitor_.initFromSettings(parser, "tankdrive:distance_action:stall_monitor");
+
+	velocity_threshold_ = parser.getDouble("tankdrive:distanceaction:velocity_threshold");
+
+	start_time_ = tank_drive_.getRobot().getTime();
+
+	tank_drive_.navx_.ZeroYaw();
 }
 
 void TankDriveVelocityAction::run() {
-	MessageLogger &logger = getTankDrive().getRobot().getMessageLogger();
+	MessageLogger &logger = tank_drive_.getRobot().getMessageLogger();
 
 	if (!is_done_) {
-		double current_velocity = getTankDrive().getVelocity();
+		double current_velocity = tank_drive_.getVelocity();
 
 		if (std::fabs(target_velocity_ - actual_velocity_) > velocity_threshold_) {
 			
-			double current_velocity = getTankDrive().getVelocity();			
+			double current_velocity = tank_drive_.getVelocity();	
+			
+			double base_power = velocity_pid_.getOutput(target_velocity_, current_velocity, tank_drive_.getRobot().getDeltaTime());
 
-			double base_power = velocity_pid_.getOutput(target_velocity_, current_velocity, getTankDrive().getRobot().getDeltaTime());
-
-			double current_angle = getTankDrive().getAngle();
-			double straightness_offset = angle_pid_.getOutput(0, current_angle, getTankDrive().getRobot().getDeltaTime());
+			double current_angle = tank_drive_.getAngle();
+			double straightness_offset = angle_pid_.getOutput(0, current_angle, tank_drive_.getRobot().getDeltaTime());
 			double left_power = base_power - straightness_offset;
 			double right_power = base_power + straightness_offset;
 
 			logger.startMessage(MessageLogger::MessageType::debug, MSG_GROUP_TANKDRIVE);
-			logger << "time " << getTankDrive().getRobot().getTime() ;
-			logger << ", target " << target_velocity;
+			logger << "time " << tank_drive_.getRobot().getTime() ;
+			logger << ", target " << target_velocity_;
 			logger << ", actual " << current_velocity ;
 			logger << ", left " << left_power << ", right " << right_power ;
 			logger.endMessage();			
 
-			getTankDrive().setMotorsToPercents(left_power, right_power);
-		} else {
+			tank_drive_.setMotorsToPercents(left_power, right_power);
+		} 
+		
+		else {
 			is_done_ = true;
 
 			logger.startMessage(MessageLogger::MessageType::debug, MSG_GROUP_TANKDRIVE);
 			logger << "TankDriveDistanceAction complete";
 			logger.startData("tankdrivedistanceaction_complete")
-				.addData("time", getTankDrive().getRobot().getTime() - start_time_)
+				.addData("time", tank_drive_.getRobot().getTime() - start_time_)
 				.endData();
 			logger.endMessage();
 		}
@@ -53,9 +75,12 @@ void TankDriveVelocityAction::cancel() {
 }
 
 bool TankDriveVelocityAction::isDone() {
-	return true;
+	return is_done_;
 }
 
 std::string TankDriveVelocityAction::toString() {
 	return "TankDriveVelocityAction " + std::to_string(target_velocity_);
+}
+
+}
 }
