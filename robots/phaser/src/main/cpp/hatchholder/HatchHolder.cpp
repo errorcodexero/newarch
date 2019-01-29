39591922b2cf16e6753392ac334c1f311ab11c65
Meg Hoots <wlnutmeg@gmail.com>
2019-01-28 20:23:44 -0800

    built hatch holder subsystem

---
+++ robots/phaser/src/main/cpp/hatchholder/HatchHolder.cpp
@@ -0,0 +1,49 @@
+#include "HatchHolder.h"
+#include <Robot.h>
+#include <ActionSequence.h>
+#include <MessageLogger.h>
+#include "HatchHolderAction.h"
+
+
+using namespace xero::base ;
+using namespace xero::misc ;
+
+namespace xero {
+    namespace phaser {
+        HatchHolder::HatchHolder(xero::base::Robot &robot) : Subsystem(robot, "HatchHolder") {
+            arm_ = std::make_shared<frc::Solenoid>(robot.getSettingsParser().getInteger("hw:hatchholder:arm"));
+            finger_ = std::make_shared<frc::Solenoid>(robot.getSettingsParser().getInteger("hw:hatchholder:finger"));
+            sensor_ = std::make_shared<frc::DigitalInput>(robot.getSettingsParser().getInteger("hw:hatchholder:sensor"));
+        }   
+
+        HatchHolder::~HatchHolder() {
+        }   
+
+        bool HatchHolder::canAcceptAction(xero::base::ActionPtr action) {
+            auto coll = std::dynamic_pointer_cast<HatchHolderAction>(action) ;
+            return coll != nullptr ;            
+
+        }  
+
+        void HatchHolder::extendArm() {
+            arm_->Set(true) ;
+        }
+
+        void HatchHolder::retractArm() {
+            arm_->Set(false) ;
+        }
+
+        void HatchHolder::extendFinger() {
+            finger_->Set(true) ;
+        }
+
+        void HatchHolder::retractFinger() {
+            finger_->Set(false) ;
+        }
+
+        bool HatchHolder::holdingHatch() {
+            return sensor_ ->Get() ;
+        }
+
+    }
+}
---
+++ robots/phaser/src/main/cpp/hatchholder/HatchHolderAction.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "Action.h"
+#include "basegroups.h"
+#include "HatchHolder.h"
+
+
+
+/// \file
+using namespace xero::base ;
+
+namespace xero {
+    namespace phaser {
+
+        /// \brief This is the base class for all actions that can be assigned to the SingleMotorSubsystem
+        /// \sa SingleMotorSubsystem
+        class HatchHolderAction : public Action {
+        
+        public:
+
+            enum Operation{
+                    EXTEND_ARM,
+                    RETRACT_ARM,
+                    EXTEND_FINGER,
+                    RETRACT_FINGER
+            };
+
+            /// \brief Create a new SingleMotorSubsystemAction
+            /// \param subsystem SingleMotor subsystem
+            HatchHolderAction(HatchHolder &subsystem, Operation operation) : subsystem_(subsystem), operation_(operation) {
+                is_done_ = false ;
+            }
+
+            ~HatchHolderAction(){
+
+            }
+
+            virtual void start() ;
+            virtual void run() ;
+            virtual bool isDone() ;
+            virtual void cancel() ;
+            virtual std::string toString() ;
+            
+        private:
+            /// The tank drive subsystem
+            HatchHolder &subsystem_;
+            Operation operation_;
+            bool is_done_;
+        };
+    }
+}
