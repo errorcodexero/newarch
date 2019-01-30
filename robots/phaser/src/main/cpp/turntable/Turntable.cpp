38f465888501b04df5303b870588a3ddcde8b2ba
Meg Hoots <wlnutmeg@gmail.com>
2019-01-29 20:51:50 -0800

    built turntable subsystem

--- robots/phaser/src/main/cpp/turntable/Turntable.cpp
+++ robots/phaser/src/main/cpp/turntable/Turntable.cpp
@@ -1,31 +1,170 @@
 #include "Turntable.h"
-using namespace xero::misc;
-using namespace xero::base;
+#include "TurntableAction.h"
+#include <Robot.h>
+#include <MessageLogger.h>
+
+
+
+using namespace xero::base ;
+using namespace xero::misc ;
 
 namespace xero{
     namespace phaser{
-        Turntable::Turntable(xero::base::Robot &robot, uint64_t id){
-            int motor = robot.getSettingsParser().getInteger("hw:turntable:motor") ;
+        Turntable::Turntable(Robot &robot, uint64_t id) : Subsystem(robot, "turntable") {
+
+            SettingsParser &parser = robot.getSettingsParser() ;
+            
+            msg_id_ = id ;
+            
+            getMotors(robot) ;
+
+
             int enc1 = robot.getSettingsParser().getInteger("hw:turntable:encoder1") ;
             int enc2 = robot.getSettingsParser().getInteger("hw:turntable:encoder2") ;
             
-            motor_ = std::make_shared<frc::VictorSP>(motor) ;
             encoder_ = std::make_shared<frc::Encoder>(enc1, enc2) ;
 
             min_angle_ = robot.getSettingsParser().getDouble("grabber:turntable:minimum") ;
             max_angle_ = robot.getSettingsParser().getDouble("grabber:turntable:maximum") ;
             degrees_per_tick_ = robot.getSettingsParser().getDouble("turntable:degrees_per_tick") ;
 
+            if (parser.isDefined("hw:turntable:limit:min_angle")) {
+                int lbottom = parser.getInteger("hw:turntable:limit:min_angle");
+                min_angle_switch_ = std::make_shared<frc::DigitalInput>(lbottom) ;
+            }
+
+            if (parser.isDefined("hw:turntable:limit:max_angle")) {
+                int ltop = parser.getInteger("hw:turntable:limit:max_angle");
+                max_angle_switch_ = std::make_shared<frc::DigitalInput>(ltop) ;
+            }
+
+
+            //
+            // The height of the turntable when it is at the bottom of travel, relative to 
+            // the floor
+            //
+            turntable_offset_ = parser.getDouble("turntable:base") ;
+            
+            //
+            // The nubmer of inches per encoder tick
+            //
+            inches_per_tick_ = parser.getDouble("turntable:inches_per_tick") ;
+
+            //
+            // The maximum height of the turntable independent of the limit switches
+            //
+            max_angle_ = parser.getDouble("turntable:max_angle") ;
+
+            //
+            // The minimum height of the turntable independent of limit switches
+            min_angle_ = parser.getDouble("turntable:min_angle") ;
+
+            // And we start without calibration
+            is_calibrated_ = false ;
+
+            // The base encoder value
+            encoder_value_ = 0 ;
+
+            // Initialize to false, only gets reset if we have a limit switch
+            is_max_angle_ = false ;
+            is_min_angle_ = false ;
         }
 
         Turntable::~Turntable() {
         }
 
-        bool Turntable::canAcceptAction(xero::base::ActionPtr action) {
+        void Turntable::getMotors(Robot &robot) {
+            int i = 1 ;
+            bool reverse = false ;
+
+            std::string revname = "hw:turntable:reversemotors" ;
+
+            if (robot.getSettingsParser().isDefined(revname)) {
+                if (robot.getSettingsParser().get(revname).isBoolean()) {
+                    if (robot.getSettingsParser().getBoolean(revname)) {
+                        reverse = true ;
+                    }
+                }
+            }
+
+            while (true) {
+                std::string motorname = "hw:turntable:motor:" + std::to_string(i) ;
+                if (!robot.getSettingsParser().isDefined(motorname))
+                    break ;
+
+                int motor = robot.getSettingsParser().getInteger(motorname) ;
+                auto talon = std::make_shared<ctre::phoenix::motorcontrol::can::TalonSRX>(motor) ;
+                talon->SetInverted(reverse) ;
+                talon->ConfigVoltageCompSaturation(12.0, 10) ;
+                talon->EnableVoltageCompensation(true) ;                
+
+                if (motors_.size() > 0)
+                    talon->Follow(*motors_.front()) ;
+
+                motors_.push_back(talon) ;
+            }
+        }
+
+        void Turntable::setMotorPower(double v) {
+            if (is_calibrated_) {
+                //
+                // If we are calibrated, we know our height.  If our height exceeds
+                // the maximum allow height and we are trying to move up, we set the
+                // power to zero
+                //
+                if (v < 0 && angle_ <= min_angle_)
+                    v = 0.0 ;
+                else if (v > 0 && angle_ >= max_angle_)
+                    v = 0.0 ;
+            }
+
+            //
+            // Now check the raw limit switch states
+            //
+            if (v > 0 && isMaxAngle())
+                v = 0.0 ;
+            else if (v < 0 && isMinAngle())
+                v = 0.0 ;
+
+            motors_.front()->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, v);
+        }
+
+        bool Turntable::canAcceptAction(ActionPtr action) {
+            auto dir_p = std::dynamic_pointer_cast<TurntableAction>(action) ;
+            if (dir_p == nullptr)
+                return false ;
+
+            return true ;
         }
 
         void Turntable::computeState(){
-            angle_ = (encoder_->Get()) * degrees_per_tick_ ;
+            encoder_value_ = encoder_->Get() ;
+            if (min_angle_switch_ != nullptr)
+                is_min_angle_ = min_angle_switch_->Get() ;
+
+            if (max_angle_switch_ != nullptr)
+                is_max_angle_ = max_angle_switch_->Get() ;
+
+            if (is_min_angle_ && calibrate_from_angle_)
+                calibrate() ;
+
+            if (is_calibrated_) {
+                angle_ = encoder_value_ * inches_per_tick_ + turntable_offset_ ;
+                speed_ = (angle_ - last_angle_) / getRobot().getDeltaTime() ;
+                last_angle_ = angle_ ;
+            }
+        }
+
+        void Turntable::calibrate() {
+            //
+            // The calibrate action assumes the turntable is at the bottom of travel
+            // This may be because the turntable put there at the start of the match
+            // and calibrate is called.  It may also be because the bottom limit
+            // has activated and we are recalibrating to the limit switch
+            //
+            is_calibrated_ = true ;
+            encoder_->Reset() ;
+            last_angle_ = turntable_offset_ ;
         }
     }
 }
