bd97e9aae2a16fd24020b7163bd7762f4e5f5e56
Jack (Butch) Griffin <butchg@comcast.net>
2019-02-05 08:21:01 -0800

    prototype for drive by vision

--- robots/phaser/src/main/cpp/phasercameratracker/DriveByVisionAction.cpp
+++ robots/phaser/src/main/cpp/phasercameratracker/DriveByVisionAction.cpp
@@ -9,21 +9,200 @@ namespace xero {
     namespace phaser {
         DriveByVisionAction::DriveByVisionAction(TankDrive &tank_drive, PhaserCameraTracker &camera) : TankDriveAction(tank_drive), camera_(camera)
         {
+            yaw_base_power_ = tank_drive.getRobot().getSettingsParser().getDouble("drivebyvision:yaw_base_power") ;
+            yaw_p_ = tank_drive.getRobot().getSettingsParser().getDouble("drivebyvision:yaw_p") ;
+            yaw_threshold_ = tank_drive.getRobot().getSettingsParser().getDouble("drivebyvision:yaw_threshold") ;
+
+            outer_dist_threshold_ = tank_drive.getRobot().getSettingsParser().getDouble("drivebyvision:outer_distance") ;
+
+            inner_boundaries_[0] = tank_drive.getRobot().getSettingsParser().getDouble("drivebyvision:inner:0") ;
+            inner_boundaries_[1] = tank_drive.getRobot().getSettingsParser().getDouble("drivebyvision:inner:1") ;
+            inner_boundaries_[2] = tank_drive.getRobot().getSettingsParser().getDouble("drivebyvision:inner:2") ;
+            inner_boundaries_[3] = tank_drive.getRobot().getSettingsParser().getDouble("drivebyvision:inner:3") ;
+
+            outer_boundaries_[0] = tank_drive.getRobot().getSettingsParser().getDouble("drivebyvision:outer:0") ;
+            outer_boundaries_[1] = tank_drive.getRobot().getSettingsParser().getDouble("drivebyvision:outer:1") ;
+            outer_boundaries_[2] = tank_drive.getRobot().getSettingsParser().getDouble("drivebyvision:outer:2") ;
+            outer_boundaries_[3] = tank_drive.getRobot().getSettingsParser().getDouble("drivebyvision:outer:3") ;  
+
+            scurve_base_power_ =  tank_drive.getRobot().getSettingsParser().getDouble("drivebyvisiion:scurve_base_power") ;
+            scurve_turn_offset_ =  tank_drive.getRobot().getSettingsParser().getDouble("drivebyvisiion:scurve_turn_offset") ;
+            scurve_cycles_ = tank_drive.getRobot().getSettingsParser().getInteger("drivebyvision:scruve_cycles") ;
         }
 
+        std::string DriveByVisionAction::toString(DriveByVisionAction::State st) {
+            std::string ret = "???" ;
+
+            switch(st) {
+            case State::FinalDriveByYaw:
+                ret = "FinalDriveByYaw" ;
+                break ;                
+            case State::InitialDriveByYaw:
+                ret = "InitialDriveByYaw" ;
+                break ;                
+            case State::DriveFirstSCurve:
+                ret = "DriveFirstSCurve" ;
+                break ;  
+            case State::DriveSecondSCurve:
+                ret = "DriveSecondSCurve" ;
+                break ;                                
+            case State::Done:
+                ret = "Done" ;
+                break ;
+            }
+
+            return ret ;
+        }
 
         void DriveByVisionAction::start() {
-            is_done_ = false ;
-            getTankDrive().lowGear() ;
+            state_ = State::InitialDriveByYaw ;
+        }
+
+        void DriveByVisionAction::driveByYaw() {
+            MessageLogger &logger = getTankDrive().getRobot().getMessageLogger() ;
+
+            double yaw = camera_.getYaw() ;
+            double yawadj = yaw * yaw_p_ ;
+            double left = yaw_base_power_ + yawadj ;
+            double right = yaw_base_power_ - yawadj ;
+            setMotorsToPercents(left, right) ;
+
+            logger.startMessage(MessageLogger::MessageType::debug, MSG_GROUP_VISION_DRIVING) ;
+            logger << "DriveByVision:" ;
+            logger << " state " << toString(state_) ;
+            logger << " yaw " << yaw ;
+            logger << " yawadj " << yawadj ;
+            logger << " left " << left ;
+            logger << " right " << right ;
+            logger.endMessage() ;
+        }
+
+        bool DriveByVisionAction::driveSCurve() {
+            bool ret = false ;
+
+            MessageLogger &logger = getTankDrive().getRobot().getMessageLogger() ;
+
+            double left = scurve_base_power_ + scurve_left_offset_ ;
+            double right = scurve_base_power_ + scurve_right_offset_ ;
+
+            setMotorsToPercents(left, right) ;
+
+            cycles_-- ;
+
+            if (cycles_ == 0)
+                ret = true ;
+
+            logger.startMessage(MessageLogger::MessageType::debug, MSG_GROUP_VISION_DRIVING) ;
+            logger << "DriveByVision:" ;
+            logger << " state " << toString(state_) ;
+            logger << " cycles " << cycles_ ;
+            logger << " left " << left ;
+            logger << " right " << right ;
+            logger.endMessage() ;
+
+            return ret ;
+        }
+
+        int DriveByVisionAction::computeQuadrant() {
+            double dist = camera_.getDistance() ;
+            double rect = camera_.getRectRatio() ;
+            int quad = -1 ;
+
+            if (dist > outer_dist_threshold_) {
+                //
+                // We are in the outer quadrants (1, 2, or 3)
+                //
+                if (rect >= outer_boundaries_[0] && rect <= outer_boundaries_[1])
+                    quad = 1 ;
+                else if (rect > outer_boundaries_[1] && rect <= outer_boundaries_[2])
+                    quad = 3 ;
+                else if (rect > outer_boundaries_[2] && rect <= outer_boundaries_[3])
+                    quad = 2 ;
+            }
+            else {
+                //
+                // We are in the inner quadrants (4, 3, 5)
+                //
+                if (rect >= inner_boundaries_[0] && rect <= inner_boundaries_[1])
+                    quad = 4 ;
+                else if (rect > inner_boundaries_[1] && rect <= inner_boundaries_[2])
+                    quad = 3 ;
+                else if (rect > inner_boundaries_[2] && rect <= inner_boundaries_[3])
+                    quad = 5 ;                
+            }
+
+            return quad ;
+        }
+
+        void DriveByVisionAction::evaluatePosition() {
+            int quad = computeQuadrant() ;
+
+            switch(quad) {
+            case 1:
+                state_ = State::DriveFirstSCurve ;
+                scurve_left_offset_ = -scurve_turn_offset_ ;
+                scurve_right_offset_ = scurve_turn_offset_ ;
+                cycles_ = scurve_cycles_ ;
+                break ;
+
+            case 2:
+                state_ = State::DriveFirstSCurve ;
+                scurve_left_offset_ = scurve_turn_offset_ ;
+                scurve_right_offset_ = -scurve_turn_offset_ ;
+                cycles_ = scurve_cycles_ ;
+                break ;            
+
+            case 3:
+                state_ = State::FinalDriveByYaw ;
+                break ;
+
+            default:
+                state_ = State::Done ;
+                break ;
+            }
         }
 
         /// \brief Manage the action; called each time through the robot loop
         void DriveByVisionAction::run() {
-            MessageLogger &logger = getTankDrive().getRobot().getMessageLogger() ;
+            double dist = camera_.getDistance() ;
 
-            logger.startMessage(MessageLogger::MessageType::debug, MSG_GROUP_VISION_DRIVING) ;
-            logger << "DriveByVision:" ;
-            logger.endMessage() ; 
+            switch(state_) {
+            case State::InitialDriveByYaw:
+                driveByYaw() ;
+                if (camera_.getYaw() < yaw_threshold_) {
+                    //
+                    // We are pointing enough at the target that we can evaluate our position
+                    //
+                    evaluatePosition() ;
+                }
+                break ;
+
+            case State::DriveFirstSCurve:
+                if (driveSCurve()) {
+                    state_ = State::DriveSecondSCurve ;
+                    scurve_left_offset_ = -scurve_left_offset_ ;
+                    scurve_right_offset_ = -scurve_right_offset_ ;
+                    cycles_ = scurve_cycles_ ;
+                }
+                break ;
+
+            case State::DriveSecondSCurve:
+                if (driveSCurve())
+                    state_ = State::FinalDriveByYaw ;
+                break ;
+
+            case State::FinalDriveByYaw:
+                driveByYaw() ;
+                if (dist < 6.0) {
+                    state_ = State::Done ;
+                    setMotorsToPercents(0.0, 0.0) ;
+                }
+                break ;
+
+            case State::Done:
+                setMotorsToPercents(0.0, 0.0) ;
+                break ;                
+            }
         }
 
         /// \brief Cancel the action
@@ -33,7 +212,7 @@ namespace xero {
         /// \brief Return true if the action is complete
         /// \returns True if the action is complete
         bool DriveByVisionAction::isDone() {
-            return is_done_ ;
+            return state_ == State::Done ;
         }
 
         /// \brief return a human readable string representing the action
--- robots/phaser/src/main/cpp/phasercameratracker/DriveByVisionAction.h
+++ robots/phaser/src/main/cpp/phasercameratracker/DriveByVisionAction.h
@@ -26,11 +26,49 @@ namespace xero {
 
             /// \brief return a human readable string representing the action
             /// \returns a human readable string representing the action
-            virtual std::string toString() ;            
+            virtual std::string toString() ;  
 
         private:
+            enum class State {
+                InitialDriveByYaw,
+                DriveFirstSCurve,
+                DriveSecondSCurve,
+                FinalDriveByYaw,
+                Done,
+            } ;            
+
+        private:
+            void driveByYaw() ;    
+            bool driveSCurve() ;
+            void evaluatePosition() ;
+            int computeQuadrant() ;
+            std::string toString(State st) ;
+
+        private:
+            State state_ ;
             PhaserCameraTracker &camera_ ;
-            bool is_done_ ;
+
+            // Drive by Yaw constants
+            double yaw_base_power_ ;
+            double yaw_p_ ;
+
+            // Threshold for evaluation quadrant
+            double yaw_threshold_ ;
+
+            // Threshold to be in the outer ring
+            double outer_dist_threshold_ ;
+
+            // Thresholds for the quadrant based on rect ratio
+            double inner_boundaries_[4] ;
+            double outer_boundaries_[4] ;
+
+            // For S curve
+            double scurve_base_power_ ;
+            double scurve_turn_offset_ ;
+            double scurve_left_offset_ ;
+            double scurve_right_offset_ ;
+            int scurve_cycles_ ;
+            int cycles_ ;
         } ;
     }
 }
