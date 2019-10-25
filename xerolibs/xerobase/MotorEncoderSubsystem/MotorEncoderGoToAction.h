7ba1ebc543686a4e640726847220a464c0a7022d
Meg Hoots <wlnutmeg@gmail.com>
2019-10-24 20:56:02 -0700

    first implementation of motor encoder subsystem on master

---
+++ xerolibs/xerobase/MotorEncoderSubsystem/MotorEncoderGoToAction.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <PIDACtrl.h>
+#include <PIDCtrl.h>
+#include <TrapezoidalProfile.h>
+
+#include "MotorEncoderSubsystemAction.h"
+#include "MotorEncoderSubsystem.h"
+
+#include <string>
+#include <memory>
+
+
+namespace xero {
+    namespace base {
+        class MotorEncoderGoToAction : public MotorEncoderSubsystemAction {
+        public:
+            MotorEncoderGoToAction(MotorEncoderSubsystem &subsystem, double target);
+
+            virtual void start();
+            virtual void run();
+            virtual bool isDone();
+            virtual void cancel();
+            virtual std::string toString();
+
+        private:
+            bool isDone_ ;
+            double threshold_;
+            double target_ ;            
+            double startTime_ ;
+            double startPosition_ ;
+            std::shared_ptr<xero::misc::PIDACtrl> ctrl_ ;
+            std::shared_ptr<xero::misc::TrapezoidalProfile> profile_ ;
+            xero::misc::PIDCtrl holdCtrl_ ;
+        };
+    } 
+}
---
+++ xerolibs/xerobase/MotorEncoderSubsystem/MotorEncoderSubsystem.cpp
@@ -0,0 +1,38 @@
+#include <Robot.h>
+
+#include "MotorEncoderSubsystem.h"
+#include "MotorEncoderSubsystemAction.h"
+
+namespace xero {
+    namespace base {
+        MotorEncoderSubsystem::MotorEncoderSubsystem(
+            Robot &robot, 
+            const std::string &name, 
+            const std::string config,
+            uint64_t id
+        ): SingleMotorSubsystem(robot, name, config + ":motor", id), configName_(name), msg_id_(id) {
+            
+            auto &settings = robot.getSettingsParser();
+            encoder_ = std::make_shared<frc::Encoder>(
+                settings.getInteger(config + ":encoder:1"),
+                settings.getInteger(config + ":encoder:2")
+            );
+
+            unitsPerTick_ = settings.getDouble(config + ":encoder:units_per_tick");
+            zeroTicks_ = settings.getInteger(config + ":encoder:zero_ticks");
+        }
+
+        double MotorEncoderSubsystem::ticksToUnits(int ticks) {
+            return (ticks - zeroTicks_) * unitsPerTick_;
+        }
+
+        bool MotorEncoderSubsystem::canAcceptAction(xero::base::ActionPtr action) {
+            return std::dynamic_pointer_cast<MotorEncoderSubsystemAction>(action) != nullptr;
+        }
+
+        void MotorEncoderSubsystem::computeState() {
+            encoderValue_ = encoder_->Get();
+            position_ = ticksToUnits(encoderValue_);
+        }
+    }
+}
