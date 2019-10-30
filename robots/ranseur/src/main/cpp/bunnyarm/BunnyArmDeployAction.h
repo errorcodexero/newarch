7564d864df2b111ed2678c49f58408d7b0358957
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2019-10-29 20:07:53 -0700

    [BunnyArmDeployAction] support both directions, add timeout

--- robots/ranseur/src/main/cpp/bunnyarm/BunnyArmDeployAction.h
+++ robots/ranseur/src/main/cpp/bunnyarm/BunnyArmDeployAction.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Robot.h>
+
 #include "BunnyArmAction.h"
 #include "BunnyArm.h"
 
@@ -7,31 +9,37 @@ namespace xero {
     namespace ranseur {
         class BunnyArmDeployAction : public BunnyArmAction {
         public:
-            BunnyArmDeployAction(BunnyArm &bunnyArm) : BunnyArmAction(bunnyArm) { 
-                is_done_ = false;               
+            /// Creates a BunnyArmDeployAction.
+            /// @param bunnyArm The bunny arm subsystem.
+            /// @param raise True if the arm should be raised, false if it should be lowered.
+            BunnyArmDeployAction(BunnyArm &bunnyArm, bool raise) : BunnyArmAction(bunnyArm), raise_(raise) { 
+                isDone_ = false;      
+                duration_ = bunnyArm.getRobot().getSettingsParser().getDouble("hw:bunnyarm:deploy:time");         
             }
             
             virtual ~BunnyArmDeployAction() {                
             }
 
             virtual void start() {                
-                is_done_ = false ;
+                isDone_ = false ;
+                BunnyArm &bunnyArm = getBunnyArm() ;
+                bunnyArm.solenoid_->Set(raise_);
+
+                // Determine the end time.
+                endTime_ = bunnyArm.getRobot().getTime() + duration_;
             }
 
             virtual void run() {
-                if (!is_done_) {
-                    BunnyArm &bunnyArm = getBunnyArm() ;
-                    bunnyArm.solenoid_->Set(true);
-                    is_done_ = true ;
-                }
+                // we're done once we pass the end time
+                isDone_ = (getBunnyArm().getRobot().getTime() > endTime_);
             }
 
             virtual bool isDone() {
-                return is_done_ ;
+                return isDone_ ;
             }
 
             virtual void cancel() {
-                is_done_ = true ;
+                isDone_ = true ;
             }
 
             virtual std::string toString() {
@@ -39,7 +47,10 @@ namespace xero {
             }
 
         private:
-            bool is_done_ ;
+            bool isDone_;
+            double endTime_;
+            double duration_;
+            bool raise_;
             static const std::string action_name ;
         } ;
     }
