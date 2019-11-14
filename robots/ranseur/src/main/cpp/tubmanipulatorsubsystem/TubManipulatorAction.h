65f465f15292e674aa8de728814aacf8ecc23fdd
Meg Hoots <wlnutmeg@gmail.com>
2019-11-13 19:58:28 -0800

    work on turtle and eject

--- robots/ranseur/src/main/cpp/tubmanipulatorsubsystem/TubManipulatorAction.h
+++ robots/ranseur/src/main/cpp/tubmanipulatorsubsystem/TubManipulatorAction.h
@@ -6,7 +6,7 @@
 
 namespace xero {
     namespace ranseur {
-        class TubManipulator ; 
+        class TubManipulatorSubsystem ; 
 
         class TubManipulatorAction : public xero::base::Action {
 
@@ -29,19 +29,19 @@ namespace xero {
                 return action_.isDone();
             }
 
-            TubManipulatorAction(TubManipulator &tubmanipulator) : tubmanipulator_(tubmanipulator) {                
+            TubManipulatorAction(TubManipulatorSubsystem &tubmanipulatorsubsytem) : tubmanipulator_(tubmanipulatorsubsytem) {                
             }
 
             virtual ~TubManipulatorAction() {                
             }
 
-            TubManipulator &getTubManipulator() {
+            TubManipulatorSubsystem &getTubManipulatorSubsystem() {
                 return tubmanipulator_ ;
             }      
 
         private:
             xero::base::ParallelAction action_ ;    
-            TubManipulator &tubmanipulator_ ;
+            TubManipulatorSubsystem &tubmanipulator_ ;
         };
     }
 }
--- robots/ranseur/src/main/cpp/tubmanipulatorsubsystem/TubManipulatorEjectAction.cpp
+++ robots/ranseur/src/main/cpp/tubmanipulatorsubsystem/TubManipulatorEjectAction.cpp
@@ -4,15 +4,33 @@
 #include <actions/Action.h>
 #include <MessageLogger.h>
 #include <Robot.h>
+#include <motorencodersubsystem/MotorEncoderGoToAction.h>
 
 using namespace xero::misc ;
+using namespace xero::base ;
 
 namespace xero {
     namespace ranseur {
 
         std::string TubManipulatorEjectAction::action_name("TubManipulatorEjectAction");
 
-        TubManipulatorEjectAction::TubManipulatorEjectAction(TubManipulator &tubm) : TubManipulatorAction(tubm) {
+        TubManipulatorEjectAction::TubManipulatorEjectAction(TubManipulatorSubsystem &tubm) : TubManipulatorAction(tubm) {
+           
+            double v ;
+            ActionPtr act ;
+            auto collector = tubm.getTubCollector() ;
+            auto arm = tubm.getTubArm() ;
+            auto wrist = tubm.getTubWrist() ;
+
+            v = tubm.getRobot().getSettingsParser().getDouble("tubarm:eject:pos") ;
+            act = std::make_shared<MotorEncoderGoToAction>(*arm, v) ;
+            parallel_.addSubActionPair(arm, act) ;
+
+
+            v =tubm.getRobot().getSettingsParser().getDouble("tubwrist:eject:pos") ;
+            act = std::make_shared<MotorEncoderGoToAction>(*wrist, v) ;
+            parallel_.addSubActionPair(wrist, act) ;            
+
         }
 
         TubManipulatorEjectAction::~TubManipulatorEjectAction() {            
