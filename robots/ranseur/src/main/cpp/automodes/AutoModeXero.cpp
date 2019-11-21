7fe3cdda8cdfa0cad321101791d8eae6c534d8d9
Aditi Bhaskar <aditi.bhaskar@gmail.com>
2019-11-20 20:13:32 -0800

    Added the 'terminate path is sees the tub' to the first parallel sequence

--- robots/ranseur/src/main/cpp/automodes/AutoModeXero.cpp
+++ robots/ranseur/src/main/cpp/automodes/AutoModeXero.cpp
@@ -5,12 +5,13 @@
 #include <tubcollector/TubCollector.h>
 #include <tubcollector/TubCollectorTubAction.h>
 #include <tubcollector/TubCollectorEjectTubAction.h>
-#include <actions/DelayAction.h>
-#include <actions/SequenceAction.h>
 #include <ranseurcameratracker/DriveByVisionAction.h>
 #include <tubmanipulatorsubsystem/TubManipulatorCollectAction.h>
 #include <tubmanipulatorsubsystem/TubManipulatorDumpAction.h>
 #include <tankdrive/actions/TankDriveFollowPathAction.h>
+#include <actions/DelayAction.h>
+#include <actions/SequenceAction.h>
+#include <actions/TerminateAction.h>
 #include <memory>
 
 using namespace xero::base ;
@@ -32,18 +33,21 @@ namespace xero
             auto tubmanipulatorsubsytem = ranseur.getRanseurRobotSubsystem()->getTubManipulatorSubsystem() ;
             auto parallel = std::make_shared<ParallelAction>() ;
             
-            // and add parallel to sequence
+            // add parallel to sequence
             // everything else contained in parallel -> 2 seq actions
             pushAction(parallel) ;
 
-            ///// 1ST SEQUENCE /////
+        //// 1ST SEQUENCE ////
             auto sequence = std::make_shared<SequenceAction>(robot.getMessageLogger()) ;
             parallel->addAction(sequence) ;
-            sequence->pushAction(std::make_shared<TankDriveFollowPathAction>(*tankdrive, "BunnyAutoMode_PathToTub")) ;
             // TBD : terminate pathfollowing if sees the tub
+            auto path = std::make_shared<TankDriveFollowPathAction>(*tankdrive, "BunnyAutoMode_PathToTub") ;
+            auto term = std::make_shared<TerminateAction>(tankdrive, path , ranseur) ;
+            term->addTerminator(camera) ; 
+            sequence->pushAction(term) ;
             sequence->pushAction(std::make_shared<DriveByVisionAction>(*tankdrive, *camera)) ;
 
-            ///// 2ND SEQUENCE /////
+        //// 2ND SEQUENCE ////
             sequence = std::make_shared<SequenceAction>(robot.getMessageLogger()) ;
             parallel->addAction(sequence) ;
             sequence->pushAction(std::make_shared<DelayAction>(5.0)) ; //this delay time is just a place holder for a constant
