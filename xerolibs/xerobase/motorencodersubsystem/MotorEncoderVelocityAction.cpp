3b589029f5f2ab2b2f0a4c1e8b6c6e69096e30a6
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2020-01-13 20:09:03 -0800

    typo

--- xerolibs/xerobase/motorencodersubsystem/MotorEncoderVelocityAction.cpp
+++ xerolibs/xerobase/motorencodersubsystem/MotorEncoderVelocityAction.cpp
@@ -66,7 +66,7 @@ namespace xero {
         {
             MotorEncoderSubsystemAction::cancel();
             setDone();
-            sub.endPlot(plotid_);
+            getSubsystem().endPlot(plotid_);
             getSubsystem().setMotor(0.0);
         }
     }
