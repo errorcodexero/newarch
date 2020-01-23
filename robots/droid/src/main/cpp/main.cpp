38f25eda7a177fc8c6272cea8cba0e9564cb3630
Jack (Butch) Griffin <butchg@comcast.net>
2020-01-22 17:06:06 -0800

    updated to put sim2 in place for the new droid code base

--- robots/droid/src/main/cpp/main.cpp
+++ robots/droid/src/main/cpp/main.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 #include "Droid.h"
 
-#ifdef SIMULATOR
-#include <droidSimulator.h>
-xero::sim::droid::droidSimulator sim("robotsims/droidsim/sim.dat") ;
+#ifdef SIM2
+#include <SimulatorEngine.h>
+#include <ModelFactory.h>
+#include <BuiltInModels.h>
+
+using namespace xero::sim2;
+using namespace xero::models;
+
+//
+// Specify the simulation models needed by this robot.  These may be in the
+// models library or may be part of this code base
+//
+USE_SIMULATION_MODEL(WestCoastDrive, "westcoastdrive");
 #endif
 
 int main(int ac, char **av)
 {
-
-#ifdef SIMULATOR
-    frc::StartRobot<xero::droid::Droid>(ac, av) ;
-#else
-    frc::StartRobot<xero::droid::Droid>() ;
+#ifdef SIM2
+    xero::sim2::SimulatorEngine &engine = xero::sim2::SimulatorEngine::getEngine();
+    if (engine.start(ac, av) != xero::sim2::SimulatorEngine::ErrorCode::NoError)
+        return 1;
 #endif
+
+    frc::StartRobot<xero::droid::Droid>() ;
+
     return 0 ;
 }
