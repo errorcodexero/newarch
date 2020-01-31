f8b2ebc6c274ca2f69477d1336d3ebd5fe396ea2
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2020-01-30 21:35:57 -0800

    add missing files

--- sim2/rev/include/rev/CANSparkMaxLowLevel.h
+++ sim2/rev/include/rev/CANSparkMaxLowLevel.h
@@ -45,14 +45,7 @@ namespace rev
             return *encoder_;
         }
 
-        void Set(double value) {
-            if (value > 1.0)
-                value = 1.0 ;
-            else if (value < -1.0)
-                value = -1.0 ;
-                
-            power_ = value ;
-        }
+        void Set(double value);
 
         double Get() const {
             return power_ ;
--- sim2/rev/src/rev/CANSparkMaxLowLevel.cpp
+++ sim2/rev/src/rev/CANSparkMaxLowLevel.cpp
@@ -1,1 +1,16 @@
 #include "CANSparkMaxLowLevel.h"
+#include <REVManager.h>
+#include <SimulatorEngine.h>
+
+void rev::CANSparkMaxLowLevel::Set(double value) {
+    if (value > 1.0)
+        value = 1.0 ;
+    else if (value < -1.0)
+        value = -1.0 ;
+        
+    power_ = value ;
+    
+    xero::sim2::SimulatorEngine &engine = xero::sim2::SimulatorEngine::getEngine();
+    auto mgr = engine.getREVManager();
+    mgr->set(GetDeviceID(), value);
+}
