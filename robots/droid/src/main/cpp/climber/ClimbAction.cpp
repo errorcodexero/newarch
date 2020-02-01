e4575eb19d4061be81c5471523695eb5b86528e3
Aditi Bhaskar <aditi.bhaskar@gmail.com>
2020-02-01 15:02:22 -0800

    Finished the Climb Action on Climber subsystem

--- robots/droid/src/main/cpp/climber/ClimbAction.cpp
+++ robots/droid/src/main/cpp/climber/ClimbAction.cpp
@@ -1,8 +1,22 @@
 
 #include "ClimbAction.h"
+#include "Climber.h"
+#include "motorencodersubsystem/MotorEncoderGoToAction.h"
+
+using namespace xero::base ;
 
 namespace xero {
     namespace droid {
+        void ClimbAction::start() {   
+        }
+        void ClimbAction::run() {
+            getClimber().getLifter()->setAction(std::make_shared<MotorEncoderGoToAction>(*getClimber().getLifter(), "climber:motor:climb")) ;
+            getClimber().getServoR()->Set(-1.0) ;               //("climber:servo:r:climb") ;
+            getClimber().getServoL()->Set(-1.0) ;               //("climber:servo:l:climb") ;   
+        }
+        std::string ClimbAction::toString() {
+            return "ClimbAction" ;
+        }
 
     }
 }
--- robots/droid/src/main/cpp/climber/ClimbAction.h
+++ robots/droid/src/main/cpp/climber/ClimbAction.h
@@ -1,10 +1,21 @@
 #pragma once
 
 #include "Climber.h"
-
+#include "ClimberAction.h"
+#include "droidids.h"
 
 namespace xero {
-    namespace base {
-        
+    namespace droid {
+           class ClimbAction : public ClimberAction {
+  
+        public:
+            ClimbAction(Climber &climber) : ClimberAction(climber) {}
+
+            virtual void start() ;
+
+            virtual void run() ;
+
+            virtual std::string toString() ;
+        };
     }
 }
