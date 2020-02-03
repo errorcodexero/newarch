e63536fd4d9ad572888aebb66934fedfc934b5f2
Jack (Butch) Griffin <butchg@comcast.net>
2020-02-03 11:23:19 -0800

    pushed in a few comments into the tests

--- robots/droid/src/main/cpp/automodes/DroidTestTuneMode.cpp
+++ robots/droid/src/main/cpp/automodes/DroidTestTuneMode.cpp
@@ -53,6 +53,11 @@ namespace xero
 
             switch(mode)
             {
+                //////////////////////////////////////////////////////////////////////////////////////////
+                //
+                // 0 - 9 drive base related
+                //
+                //////////////////////////////////////////////////////////////////////////////////////////
             case 0:     // Drive base straight characterization
                 pushSubActionPair(tankdrive, std::make_shared<TankDriveCharAction>(*tankdrive, duration, power, true)) ;
                 break;
@@ -61,39 +66,76 @@ namespace xero
                 pushSubActionPair(tankdrive, std::make_shared<TankDriveScrubCharAction>(*tankdrive, power, duration, true));
                 break;
 
-            case 2:     // Test the collector
+                //////////////////////////////////////////////////////////////////////////////////////////
+                //
+                // 10 - 19 intake related
+                //
+                //////////////////////////////////////////////////////////////////////////////////////////
+            case 10:
+                pushSubActionPair(game->getIntake(), std::make_shared<SingleMotorPowerAction>(*game->getIntake(), power, duration));
+                break;
+
+            case 11:     // Test the collector
                 pushSubActionPair(game->getIntake(), std::make_shared<CollectOnAction>(*game->getIntake()));
                 pushAction(std::make_shared<DelayAction>(droid.getMessageLogger(), 3.0));
                 // pushSubActionPair(game->getIntake(), std::make_shared<CollectOffAction>(*game->getIntake()));                
                 break;
 
-            case 3:     // Test the conveyor - just turn it on
+                //////////////////////////////////////////////////////////////////////////////////////////
+                //
+                // 20 - 29 conveyor related
+                //
+                //////////////////////////////////////////////////////////////////////////////////////////
+            case 20:     // Test the conveyor - just turn it on
                 pushSubActionPair(game->getConveyor(), std::make_shared<ConveyorOnAction>(*game->getConveyor()));
                 break;
 
-            case 4:     // Test the conveyor - collect path
+            case 21:     // Test the conveyor - collect path
                 pushSubActionPair(game->getConveyor(), std::make_shared<ConveyorPrepareToReceiveAction>(*game->getConveyor()));            
                 pushSubActionPair(game->getConveyor(), std::make_shared<ConveyorReceiveAction>(*game->getConveyor()));   
                 pushAction(std::make_shared<DelayAction>(droid.getMessageLogger(), 5.0)); 
                 pushSubActionPair(game->getConveyor(), std::make_shared<ConveyorStopAction>(*game->getConveyor()));
                 break;
 
-            case 5:     // Test the conveyor - shoot path
+            case 22:     // Test the conveyor - shoot path
                 pushSubActionPair(game->getConveyor(), std::make_shared<ConveyorPrepareToEmitAction>(*game->getConveyor()));            
                 pushSubActionPair(game->getConveyor(), std::make_shared<ConveyorEmitAction>(*game->getConveyor()));   
                 pushAction(std::make_shared<DelayAction>(droid.getMessageLogger(), 5.0)); 
                 pushSubActionPair(game->getConveyor(), std::make_shared<ConveyorStopAction>(*game->getConveyor()));
                 break;
 
-            case 6:     // Test the shooter
+                //////////////////////////////////////////////////////////////////////////////////////////
+                //
+                // 30 - 39 shooter related
+                //
+                //////////////////////////////////////////////////////////////////////////////////////////
+            case 30:     // Test the shooter
                 pushSubActionPair(game->getShooter(), std::make_shared<FireAction>(*game->getShooter(), 7000.0));
                 pushAction(std::make_shared<DelayAction>(droid.getMessageLogger(), 5.0)); 
                 pushSubActionPair(game->getShooter(), std::make_shared<FireAction>(*game->getShooter(), 0.0));                                
                 break;
 
-            case 7:     // Shoot balls
+            case 31:     // Shoot balls
                 pushSubActionPair(game, std::make_shared<ShootTestingAction>(*game));
                 break;
+
+                //////////////////////////////////////////////////////////////////////////////////////////
+                //
+                // 40 - 49 control panel spinner related
+                //
+                //////////////////////////////////////////////////////////////////////////////////////////
+
+                //////////////////////////////////////////////////////////////////////////////////////////
+                //
+                // 50 - 59 climber related
+                //
+                //////////////////////////////////////////////////////////////////////////////////////////
+
+                //////////////////////////////////////////////////////////////////////////////////////////
+                //
+                // 60 - 69 buddy climber related
+                //
+                //////////////////////////////////////////////////////////////////////////////////////////                
             }
         }
 
