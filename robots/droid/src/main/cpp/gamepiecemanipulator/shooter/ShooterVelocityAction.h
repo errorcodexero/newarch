f5ad478ae515db3c5bf4db9e9249fb4b85db0b1d
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2020-02-03 19:26:06 -0800

    add missing files

---
+++ robots/droid/src/main/cpp/gamepiecemanipulator/shooter/ShooterVelocityAction.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <motorencodersubsystem/MotorEncoderVelocityAction.h>
+#include <networktables/NetworkTableEntry.h>
+#include <frc/shuffleboard/Shuffleboard.h>
+
+namespace xero {
+    namespace droid {
+        class Shooter ;
+
+        class ShooterVelocityAction : public xero::base::MotorEncoderVelocityAction {
+
+        public:
+
+            ShooterVelocityAction(Shooter &sub) ;
+            virtual ~ShooterVelocityAction() ;
+
+             Shooter &getSubsystem() {
+                return subsystem_ ;
+            }
+     
+            virtual void run() override ;
+
+            virtual void cancel() override ;
+
+        private:
+
+            Shooter &subsystem_;
+            double ready_margin_percent_;
+        };
+    }
+}
