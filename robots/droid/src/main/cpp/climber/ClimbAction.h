99a8be1335d58a6991a6f04f029125cc7eda4742
Aditi Bhaskar <aditi.bhaskar@gmail.com>
2020-02-12 19:22:31 -0800

    ClimberActions now have trapezoidal speed profile variables in .dat file

--- robots/droid/src/main/cpp/climber/ClimbAction.h
+++ robots/droid/src/main/cpp/climber/ClimbAction.h
@@ -2,20 +2,24 @@
 
 #include "Climber.h"
 #include "ClimberAction.h"
-#include "droidids.h"
+#include <TrapezoidalProfile.h>
+#include <PIDACtrl.h>
 
 namespace xero {
     namespace droid {
            class ClimbAction : public ClimberAction {
   
         public:
-            ClimbAction(Climber &climber) : ClimberAction(climber) {}
+            ClimbAction(Climber &climber) ;
 
             virtual void start() ;
 
             virtual void run() ;
 
             virtual std::string toString() ;
+        
+        private:
+        
         };
     }
 }
