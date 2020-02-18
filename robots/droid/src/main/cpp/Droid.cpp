9a581f2f1e5053d9007e12e5da51fa400f2ad2bf
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2020-02-17 19:55:55 -0800

    a few tweaks

--- robots/droid/src/main/cpp/Droid.cpp
+++ robots/droid/src/main/cpp/Droid.cpp
@@ -60,8 +60,7 @@ namespace xero {
             logger.enableSubsystem(MSG_GROUP_ACTIONS);  
             logger.enableSubsystem(MSG_GROUP_ACTIONS_VERBOSE);    
             logger.enableSubsystem(MSG_GROUP_OI) ;
-            logger.enableSubsystem(MSG_GROUP_TANKDRIVE);
-            logger.enableSubsystem(MSG_GROUP_TURRET); 
+            logger.enableSubsystem(MSG_GROUP_TANKDRIVE); 
 
             //
             // This should stay on.  It will have no effect on the real robot
