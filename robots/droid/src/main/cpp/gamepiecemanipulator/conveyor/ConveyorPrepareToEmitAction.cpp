64cb7c8e1043aa066732a9ffc447c9b503d69050
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2020-02-29 10:33:36 -0800

    fix PrepareToEmitAction bug

--- robots/droid/src/main/cpp/gamepiecemanipulator/conveyor/ConveyorPrepareToEmitAction.cpp
+++ robots/droid/src/main/cpp/gamepiecemanipulator/conveyor/ConveyorPrepareToEmitAction.cpp
@@ -7,17 +7,19 @@ namespace xero {
             ConveyorStateAction(subsystem, "ConveyorPrepareToEmitAction") {
             
             const std::string done = "done";
+            const std::string setStaged = "setStaged";
             setStates({
                 // if empty or already staged for fire, stop
                 branchState(done, [=] { return getSubsystem().isEmpty(); }),
                 branchState(done, [=] { return getSubsystem().isStagedForFire(); }),
+                branchState(setStaged, [=] { return getSubsystem().isFull(); }),
 
                 
                 // move balls in position towards shooter
                 setMotorState(MotorState::MoveTowardsShooter),
                 { "wait for ball to reach target sensor", waitForSensorEdgeState(Sensor::D, false) },
                 
-                { [=]{ 
+                {setStaged,  [=]{ 
                     setStagedForCollect(false); 
                     setStagedForFire(true);
                     setCollecting(false);
